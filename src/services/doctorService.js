6f077f9979398053ddb9e10f9258231ae4731911
Nguyenhoangphuong <khangnguyen8196@gmail.com>
2022-10-25 21:05:08 +0700

    bulk-create

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,4 +1,8 @@
 import db from '../models/index';
+require('dotenv').config();
+import _ from 'lodash';
+
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 let getTopDoctorHome =(limitInput) => {
     return new Promise(async(resolve, reject) => {
         try {
@@ -48,18 +52,33 @@ let getAllDoctors = () => {
 let saveDetailInforDoctor = (inputData) => {
     return new Promise(async(resolve, reject) => {
         try {
-            if (!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown){
+            if (!inputData.doctorId || !inputData.contentHTML 
+                || !inputData.contentMarkdown || !inputData.action){
                 resolve({
                     errCode:1,
                     errMessage:'Missing parameter'
                 })
             } else {
-                await db.Markdown.create({
-                    contentHTML: inputData.contentHTML,
-                    contentMarkdown: inputData.contentMarkdown,
-                    description: inputData.description,
-                    doctorId: inputData.doctorId,
-                })
+                if(inputData.action ==='CREATE'){
+                    await db.Markdown.create({
+                        contentHTML: inputData.contentHTML,
+                        contentMarkdown: inputData.contentMarkdown,
+                        description: inputData.description,
+                        doctorId: inputData.doctorId,
+                    })
+                } else if(inputData.action ==='EDIT'){
+                    let doctorMarkdown = await db.Markdown.findOne({
+                        where:{doctorId: inputData.doctorId},
+                        raw: false
+                    })
+                    if(doctorMarkdown){
+                        doctorMarkdown.contentHTML = inputData.contentHTML;
+                        doctorMarkdown.contentMarkdown = inputData.contentMarkdown;
+                        doctorMarkdown.description = inputData.description;
+                        await doctorMarkdown.save();
+                    }
+                }
+                
                 resolve({
                     errCode:0,
                     errMessage:' Save infor doctor success'
@@ -106,9 +125,60 @@ let getDetailDoctorById = (inputId) => {
         }
     })
 }
+
+let bulkCreateSchedule = (data) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            if(!data.arrSchedule || !data.doctorId || !data.formatedDate){
+                resolve({
+                    errCode:1,
+                    errMessage:' Missing required parameters!'
+                })
+            } else {
+                let schedule = data.arrSchedule;
+                if(schedule && schedule.length >0) {
+                    schedule = schedule.map(item =>{
+                        item.maxNumber = MAX_NUMBER_SCHEDULE;
+                        return item;
+                    })
+                }
+                //get all existing data
+                let existing = await db.Schedule.findAll({
+                    where:{doctorId:data.doctorId, date:data.formatedDate},
+                    attributes:['timeType', 'date', 'doctorId', 'maxNumber'],
+                    raw: true
+                })
+
+                // convert date
+                if(existing && existing.length >0){
+                    existing = existing.map(item => {
+                        item.date =new Date(item.date).getTime();
+                        return item;
+                    })
+                }
+                // compare different
+                let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+                    return a.timeType === b.timeType && a.date === b.date;
+                });
+
+                // create data 
+                if(toCreate && toCreate.length >0) {
+                    await db.Schedule.bulkCreate(toCreate);
+                }
+                resolve({
+                    errCode:0,
+                    errMessage:'OK'
+                })
+            } 
+        }catch (e) {
+            reject(e)
+        }
+    })
+}
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInforDoctor: saveDetailInforDoctor,
     getDetailDoctorById: getDetailDoctorById,
+    bulkCreateSchedule: bulkCreateSchedule,
 }
