46e0edc7f66053b62c1a99879f67853b26f3ebcc
Nguyenhoangphuong <khangnguyen8196@gmail.com>
2022-11-01 15:09:41 +0700

    xac_nhan_lich_hen_qua_Email

--- src/models/user.js
+++ src/models/user.js
@@ -15,6 +15,9 @@ module.exports = (sequelize, DataTypes) => {
 
       User.hasOne(models.Markdown, {foreignKey: 'doctorId'})
       User.hasOne(models.Doctor_Infor, {foreignKey: 'doctorId'})
+
+      User.hasMany(models.Schedule, { foreignKey: 'id', targetKey: 'doctorId', as: 'doctorData' })
+
     }
   };
   User.init({
